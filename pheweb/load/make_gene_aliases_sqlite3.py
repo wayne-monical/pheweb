ec13d897e02121782eb75c5626fab7508b2cf846
wayne-monical <wem2121@cumc.columbia.edu>
2025-06-01 14:52:53 -0400

    fixing url
    thanks to 
    https://github.com/statgen/pheweb/commit/84cda612afdf72133bed210b46d8a2f35d0f98df

--- pheweb/load/make_gene_aliases_sqlite3.py
+++ pheweb/load/make_gene_aliases_sqlite3.py
@@ -11,7 +11,8 @@ from typing import List, Dict, Iterable
 
 def get_genenamesorg_ensg_aliases_map(ensgs_to_consider: Iterable[str]) -> Dict[str, List[str]]:
     ensgs_to_consider = set(ensgs_to_consider)
-    r = urllib.request.urlopen('http://ftp.ebi.ac.uk/pub/databases/genenames/new/json/non_alt_loci_set.json')
+    # r = urllib.request.urlopen('http://ftp.ebi.ac.uk/pub/databases/genenames/new/json/non_alt_loci_set.json')
+    r = urllib.request.urlopen('https://storage.googleapis.com/public-download-files/hgnc/json/json/non_alt_loci_set.json')
     data = r.read().decode('utf-8')
     ensg_to_aliases = {}
     for row in json.loads(data)['response']['docs']:
